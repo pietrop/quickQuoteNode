c22c75e87e6cb837216ee44c4abd2d6b8242085e
pietrop <pietro.passarelli@gmail.com>
2015-11-26 19:56:36 +0000

    working version with twitter user autentication

--- components/db.js
+++ components/db.js
@@ -1,2 +1,21 @@
 // db.js
 //crud with mongo?
+
+
+// getting-started.js
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  // yay!
+
+
+
+
+});
+
+
+// http://mherman.org/blog/2013/11/10/social-authentication-with-passport-dot-js/#register-oauth
+// https://github.com/mjhea0/passport-examples

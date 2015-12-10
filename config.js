d9ceb1a18d143f8afe2af640be2cc7995c9cc07a
pietrop <pietro.passarelli@gmail.com>
2015-12-10 13:19:11 +0000

    trying to add ffmpeg-fluent module

--- config.js
+++ config.js
@@ -1,40 +1,51 @@
 /**
-* Load ENV from app.json, dev once into local system
-* production once into heroku
+* if development load ENV from config.json
+* if production do nothing, as in production these are stored in heroku ENV
 */
-// var fs = require('fs');
-// var enviromentVariables = fs.readFileSync("config.json",'utf8');
-// configJson = JSON.parse(enviromentVariables);
+var env = process.env.NODE_ENV || 'development';
+if (env == "development"){
+	console.log("env-");
+	console.log(env);	
 
-/**
-* assign ENV variables to development system enviroment.
-*/
-// var developmentKeys= configJson['development'];
-// for (var key in developmentKeys){
-// 	process.env[key]=developmentKeys[key];
-// }
+	/**
+	* Load ENV from app.json, dev once into local system
+	* production once into heroku
+	*/
+	var fs = require('fs');
+	var enviromentVariables = fs.readFileSync("config.json",'utf8');
+	configJson = JSON.parse(enviromentVariables);
+
+	/**
+	* assign ENV variables to development system enviroment.
+	*/
+	var developmentKeys= configJson['development'];
+	for (var key in developmentKeys){
+		process.env[key]=developmentKeys[key];
+	}
 
 
-/** 
-* Heroku Set ENV variables
-* Loop through JSON that has ENV keys
-* and uses Library to make system calls from javascript
-*/
-// var exec = require('child_process').exec;
-// var productionKeys = configJson['production'];
-// for (var key in productionKeys){
-// 	var value = productionKeys[key];
-// 	// system call, to heroku to set ENV variables
-// 	exec("heroku config:set "+key+"="+value);
-// }
+	/** 
+	* Heroku Set ENV variables
+	* Loop through JSON that has ENV keys
+	* and uses Library to make system calls from javascript
+	*/
+	var exec = require('child_process').exec;
+	var productionKeys = configJson['production'];
+	for (var key in productionKeys){
+		var value = productionKeys[key];
+		// system call, to heroku to set ENV variables
+		exec("heroku config:set "+key+"="+value);
+	}
+
+}
+
+
 
 
 /**
 * Make config with ENV available to app.
 * To make it convinient to work with ENV var in the app.
 */
-console.log(process.env.NODE_ENV);
-console.log(process.env.MONGOLAB_URI);
 
 var config = {};
 
